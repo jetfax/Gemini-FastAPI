f8641588462655597ea1803b385e02116a1b414e
jetfax <jetfax@gmail.com>
2026-04-21 17:17:27 -0400

    add secure_1psidcc

--- app/services/pool.py
+++ app/services/pool.py
@@ -26,6 +26,7 @@ class GeminiClientPool(metaclass=Singleton):
                 client_id=c.id,
                 secure_1psid=c.secure_1psid,
                 secure_1psidts=c.secure_1psidts,
+                secure_1psidcc=getattr(c, "secure_1psidcc", None), # <-- ADD THIS LINE
                 proxy=c.proxy,
             )
             self._clients.append(client)
